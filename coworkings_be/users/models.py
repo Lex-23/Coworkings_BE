20b3caf47c22868a9c5f7eb626f8f1e95f0f2675
Aliaksei Valenta <liverpoolsmile23@gmail.com>
2022-10-31 18:29:08 +0400

    remove UPLOAD_TO from User model

--- coworkings_be/users/models.py
+++ coworkings_be/users/models.py
@@ -1,12 +1,8 @@
-import os
-
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from users.managers import CustomUserManager
-
-USER_IMAGE_UPLOAD_DIR = os.environ["USER_IMAGE_UPLOAD_DIR"]
 
 
 class UserRoles(models.IntegerChoices):
