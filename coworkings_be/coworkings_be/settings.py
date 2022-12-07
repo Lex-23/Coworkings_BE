5241ba8271fc5aa1f24e0c4ea21d54ed99f979cb
Aliaksei Valenta <liverpoolsmile23@gmail.com>
2022-12-07 13:27:31 +0400

    setup CORS

--- coworkings_be/coworkings_be/settings.py
+++ coworkings_be/coworkings_be/settings.py
@@ -46,9 +46,11 @@ INSTALLED_APPS = [
     "working_spaces",
     "reservation",
     "utils",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -56,6 +58,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = "coworkings_be.urls"
